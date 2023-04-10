fef4e783300ebbae4207fe0bd287d7819164af45
Shad0wAVM <arturvasco.martins@gmail.com>
2023-04-10 17:41:47 +0100

    Media e desvio padrao

--- EX1/EX1.R
+++ EX1/EX1.R
@@ -3,6 +3,9 @@ library(readxl)
 econ <- read_excel("econ.xlsx", col_types = c("date", 
                                               "skip", "numeric", "numeric", "skip", 
                                               "skip"))
-econ[econ$tempo >= "1989-01-01", ]
-
+econcut <- econ[econ$tempo >= "1989-01-01", ]
+popavg <- median(econcut$pop)
+tppavg <- median(econcut$tpp)
+pops <- sd(econcut$pop)
+tpps <- sd(econcut$tpp)
 
