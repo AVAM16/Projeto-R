577b5f1a238dcad19247f2bec258becded2ef2d7
Shad0wAVM <arturvasco.martins@gmail.com>
2023-04-15 15:36:55 +0100

    Ex 1 feito falta parte estetica

--- EX1/EX1.R
+++ EX1/EX1.R
@@ -9,3 +9,18 @@ tppavg <- median(econcut$tpp)
 pops <- sd(econcut$pop)
 tpps <- sd(econcut$tpp)
 
+zpop <- function(pop) {
+  x <- (pop - popavg) / pops
+  return(x)
+}
+
+ztpp <- function(tpp) {
+  x <- (tpp - tppavg) / tpps
+  return(x)
+}
+
+z1 <- sapply(econcut$pop, zpop)
+z2 <- sapply(econcut$tpp, ztpp)
+graph1 <- ggplot(econcut, aes(tempo)) + geom_line(aes(y = z1, color = "z1")) + 
+  geom_line(aes(y= z2, color = "z2"))
+graph1 + xlab("Data") + ylab("X") 
