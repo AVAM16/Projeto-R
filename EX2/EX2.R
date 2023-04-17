2fd91545f330e3d6efa6d1ce6d9ff3bcf3ec7cfe
Shad0wAVM <arturvasco.martins@gmail.com>
2023-04-17 19:13:27 +0100

    Dados preparados EX2

---
+++ EX2/EX2.R
@@ -0,0 +1,6 @@
+library(ggplot2)
+
+TIME_USE <- read.csv("TIME_USE_24092022.csv", encoding='utf-8')
+TIME_USE <- TIME_USE[TIME_USE$País != "África do Sul",]
+TIME_CUT <- TIME_USE[TIME_USE$Sexo == "Mulheres" 
+                     & TIME_USE$Ocupação %in% c("Outros", "Trabalho remunerado ou estudo"),]
