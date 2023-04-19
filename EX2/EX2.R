41695b05c072a48f02b7f22896a285d22df83351
Shad0wAVM <arturvasco.martins@gmail.com>
2023-04-19 11:02:15 +0100

    Concluido

--- EX2/EX2.R
+++ EX2/EX2.R
@@ -4,3 +4,6 @@ TIME_USE <- read.csv("TIME_USE_24092022.csv", encoding='utf-8')
 TIME_USE <- TIME_USE[TIME_USE$País != "África do Sul",]
 TIME_CUT <- TIME_USE[TIME_USE$Sexo == "Mulheres" 
                      & TIME_USE$Ocupação %in% c("Outros", "Trabalho remunerado ou estudo"),]
+ggplot(TIME_CUT, aes(x = TIME_CUT$Ocupação, y = TIME_CUT$Tempo)) + geom_boxplot() +
+  xlab("Ocupação") +
+  ylab("Tempo")
