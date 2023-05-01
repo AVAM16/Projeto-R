75332179af819726bf21d1bdd9fb42bf9c84fa1c
Shad0wAVM <arturvasco.martins@gmail.com>
2023-05-01 16:29:49 +0100

    EX3 concluido

--- EX3/EX3.R
+++ EX3/EX3.R
@@ -7,3 +7,8 @@ dados <- dados[dados$Country == "Germany"
                & dados$SEX %in% c("MEN", "WOMEN")
                & dados$IND == "EMP5"
                & dados$AGE %in% c("1524", "2554", "5564"),]
+ggplot(dados, aes(x = dados$Age.Group, y = dados$Value, fill = dados$Sex)) + geom_bar(position = "dodge", stat="identity") + 
+  ggtitle("EMP5 value by age group and sex, 2015, Germany") +
+  xlab("Age group") +
+  ylab("EMP5 value") +
+  labs(fill = "Sex")
