44dd1ff28ed8ec31ce6647e3dde1696d8560816e
Shad0wAVM <arturvasco.martins@gmail.com>
2023-06-08 23:56:29 +0100

    EX8 e EX9 arranjados

--- EX8/EX8.R
+++ EX8/EX8.R
@@ -4,27 +4,28 @@ set.seed(1237)
 
 n <- 187
 cauchy_sample <- rcauchy(n, location = 2, scale = 1.8)
+sorted_cauchy_sample <- sort(cauchy_sample)
 
 normal_sample <- rnorm(n, mean = 3.3, sd = 2)
 
-df_cauchy <- data.frame(
-  values = sort(cauchy_sample),
-  quantiles = seq(1, n) / (n + 1)
-)
+probabilities <- seq(1, n) / (n + 1)
 
-df_normal <- data.frame(
-  values = sort(normal_sample),
-  quantiles = seq(1, n) / (n + 1)
-)
+quantiles_cauchy <- quantile(sorted_cauchy_sample, probabilities)
+
+quantiles_normal <- quantile(normal_sample, probabilities)
+
 
 plot <- ggplot() +
-  geom_point(data = df_cauchy, aes(x = quantiles, y = values),
-             color = "blue", alpha = 0.6) +
-  geom_point(data = df_normal, aes(x = quantiles, y = values),
-             color = "red", alpha = 0.6) +
+  geom_point(data = df_cauchy, aes(x = sorted_cauchy_sample, y = quantiles_cauchy,
+                                   color = "Cauchy")) +
+  geom_point(data = df_normal, aes(x = sorted_cauchy_sample, y = quantiles_normal,
+                                   color = "Normal")) +
   geom_abline(slope = 1, intercept = 0, linetype = "dashed") +
-  scale_x_continuous(name = "Quantis", limits = c(0, 1)) +
-  scale_y_continuous(name = "Valores") +
-  theme_minimal()
+  scale_x_continuous(name = "Valores da distribuição", limits = c(-100, 100)) +
+  scale_y_continuous(name = "Quantis de probabilidade", limits = c(-100, 100)) +
+  labs(color = "Quantis") +
+  scale_color_manual(values = c("blue", "red"),
+                     labels = c("Cauchy", "Normal"))
+
 
 plot
--- EX9/EX9.R
+++ EX9/EX9.R
@@ -1,42 +1,39 @@
-# Fixing the seed
+library(ggplot2)
 set.seed(1094)
 
-# Parameters
 p <- 0.2
 gamma <- 0.98
 k <- 2500
 sample_sizes <- c(30, 50, 100, 200, 300, 500, 1000)
 
-# Function to calculate the difference in CI lengths
 calculate_difference <- function(sample_size) {
-  # Method 1
+  # Metodo 1
   x_bar <- mean(rbinom(sample_size, 1, p))
   z <- qnorm((1 + gamma) / 2)
   p_1 <- (-2 * x_bar + sqrt(4 * x_bar^2 - 4 * (x_bar^2 - (z^2) * (x_bar * (1 - x_bar)) / sample_size))) / 2
   
-  # Method 2
+  # Metodo 2
   z_2 <- (x_bar - p) / sqrt((x_bar * (1 - x_bar)) / sample_size)
   
-  # Confidence intervals
   ci_1 <- c(x_bar - qnorm(gamma/2) * sqrt(x_bar * (1 - x_bar) / sample_size), 
             x_bar + qnorm(gamma/2) * sqrt(x_bar * (1 - x_bar) / sample_size))
   ci_2 <- c(p - qnorm(gamma/2) * sqrt(p * (1 - p) / sample_size), 
             p + qnorm(gamma/2) * sqrt(p * (1 - p) / sample_size))
   
-  # Difference in CI lengths
   diff_length <- abs(diff(ci_2)) - abs(diff(ci_1))
   
   return(diff_length)
 }
 
-# Calculate differences for each sample size
 mean_diff_lengths <- sapply(sample_sizes, function(n) {
   diffs <- replicate(k, calculate_difference(n))
   mean(diffs)
 })
 
-# Plotting the results
-plot(sample_sizes, mean_diff_lengths, type = "b", pch = 16,
-     xlab = "Sample Size", ylab = "Mean Difference in CI Lengths",
-     main = "Comparison of Confidence Interval Methods",
-     ylim = range(mean_diff_lengths, 0))
+plot_data <- data.frame(sample_size = sample_sizes, mean_diff_length = mean_diff_lengths)
+
+ggplot(plot_data, aes(x = sample_size, y = mean_diff_length)) +
+  geom_point(shape = 16) +
+  labs(x = "n", y = "Média das Diferenças",
+       title = "Média das diferenças em função da dimensão da amostra") +
+  ylim(0, max(mean_diff_lengths))
