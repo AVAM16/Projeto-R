b1e05da7681029e79c77cd443ecbd23c7b9d3785
Shad0wAVM <arturvasco.martins@gmail.com>
2023-06-09 22:16:44 +0100

    EX8 another change

--- EX8/EX8.R
+++ EX8/EX8.R
@@ -14,18 +14,17 @@ quantiles_cauchy <- quantile(sorted_cauchy_sample, probabilities)
 
 quantiles_normal <- quantile(normal_sample, probabilities)
 
+df <- data.frame(sample = sorted_cauchy_sample,
+                 y_cauchy = quantiles_cauchy,
+                 y_normal = quantiles_normal)
 
-plot <- ggplot() +
-  geom_point(data = df_cauchy, aes(x = sorted_cauchy_sample, y = quantiles_cauchy,
-                                   color = "Cauchy")) +
-  geom_point(data = df_normal, aes(x = sorted_cauchy_sample, y = quantiles_normal,
-                                   color = "Normal")) +
+plot <- ggplot(df, aes(x = sample)) +
+  geom_point(aes(y = y_cauchy, colour = "Cauchy")) +
+  geom_point(aes(y = y_normal, colour = "Normal")) +
   geom_abline(slope = 1, intercept = 0, linetype = "dashed") +
-  scale_x_continuous(name = "Valores da distribuição", limits = c(-100, 100)) +
-  scale_y_continuous(name = "Quantis de probabilidade", limits = c(-100, 100)) +
-  labs(color = "Quantis") +
-  scale_color_manual(values = c("blue", "red"),
-                     labels = c("Cauchy", "Normal"))
+  scale_x_continuous(name = "Valores da distribuição", limits = c(-150, 150)) +
+  scale_y_continuous(name = "Quantis de probabilidade", limits = c(-150, 150)) +
+  labs(color = "Quantis")
 
 
 plot
