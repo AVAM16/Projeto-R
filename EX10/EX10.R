b0c1de2036c73070a8c787e674b190d17ddfe563
Shad0wAVM <arturvasco.martins@gmail.com>
2023-06-14 19:15:42 +0100

    Ex10 fix

--- EX10/EX10.R
+++ EX10/EX10.R
@@ -1,22 +1,21 @@
-mu <- 51.8
-sigma <- 2
-n <- 46
-m <- 250
-
 set.seed(189)
 
-samples <- matrix(rnorm(n * m, mean = mu, sd = sigma), nrow = n, ncol = m)
+m <- 250
+n <- 46 
+mu_h0 <- 50.5
+mu <- 51.8
+sigma <- 2 
 
-hypothesis_test <- function(sample) {
-  t_stat <- (mean(sample) - 50.5) / (sigma/sqrt(n))
-  p_value <- 2 * pt(abs(t_stat), df = n-1, lower.tail = FALSE)
-  return(p_value <= 0.09)
+perform_hypothesis_test <- function(sample) {
+  t_stat <- (mean(sample) - mu_h0) / (sigma / sqrt(n))
+  p_value <- 2 * (1 - pt(abs(t_stat), df = n - 1))
+  return(p_value)
 }
 
-results <- apply(samples, 2, hypothesis_test)
+samples <- replicate(m, rnorm(n, mean = mu, sd = sigma))
 
-prob_non_rejection <- mean(results)
+p_values <- apply(samples, 2, perform_hypothesis_test)
 
-prob_non_rejection <- round(prob_non_rejection, 3)
+non_rejection_prob <- mean(p_values > 0.09)
 
-prob_non_rejection
+round(non_rejection_prob, 3)
